1efc8cc162ce6779722711e51abd08a720f2918a
Abhishek-1310 <kumaryadavabhi1999@gmail.com>
2025-06-19 04:59:13 +0530

    changed login aws-sdk

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
-const AWS = require('aws-sdk');
-const cognito = new AWS.CognitoIdentityServiceProvider();
+const {
+    CognitoIdentityProviderClient,
+    InitiateAuthCommand,
+} = require("@aws-sdk/client-cognito-identity-provider");
+
+const client = new CognitoIdentityProviderClient();
 
 exports.handler = async (event) => {
     try {
@@ -9,38 +13,38 @@ exports.handler = async (event) => {
         if (!username || !password) {
             return {
                 statusCode: 400,
-                body: JSON.stringify({ error: 'Username and password are required' })
+                body: JSON.stringify({ error: 'Username and password are required' }),
             };
         }
 
-        const params = {
-            AuthFlow: 'USER_PASSWORD_AUTH',
+        const command = new InitiateAuthCommand({
+            AuthFlow: "USER_PASSWORD_AUTH",
             ClientId: process.env.CLIENT_ID,
             AuthParameters: {
                 USERNAME: username,
-                PASSWORD: password
-            }
-        };
+                PASSWORD: password,
+            },
+        });
 
-        const authResult = await cognito.initiateAuth(params).promise();
+        const response = await client.send(command);
 
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'Login successful',
-                idToken: authResult.AuthenticationResult.IdToken,
-                accessToken: authResult.AuthenticationResult.AccessToken
-            })
+                message: "Login successful",
+                idToken: response.AuthenticationResult.IdToken,
+                accessToken: response.AuthenticationResult.AccessToken,
+            }),
         };
     } catch (err) {
         const errorMessage =
-            err.code === 'NotAuthorizedException' || err.code === 'UserNotFoundException'
-                ? 'Invalid username or password'
+            err.name === "NotAuthorizedException" || err.name === "UserNotFoundException"
+                ? "Invalid username or password"
                 : err.message;
 
         return {
             statusCode: 401,
-            body: JSON.stringify({ error: errorMessage })
+            body: JSON.stringify({ error: errorMessage }),
         };
     }
 };
